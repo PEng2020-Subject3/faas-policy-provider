e04385d713e5a62cd45776dc9c3ea02c7e12bcf0
stendler <m.stendler@fu-berlin.de>
2020-07-12 00:43:14 +0200

    add helper function to merge a map
    Signed-off-by: stendler <m.stendler@fu-berlin.de>

--- types/helper.go
+++ types/helper.go
@@ -3,9 +3,15 @@ package types
 // From https://stackoverflow.com/a/15323988
 func StringInSlice(a string, list []string) bool {
 	for _, b := range list {
-			if b == a {
-					return true
-			}
+		if b == a {
+			return true
+		}
 	}
 	return false
+}
+
+func MergeMap(a map[string]string, b map[string]string) {
+	for k, v := range b {
+		a[k] = v
+	}
 }
