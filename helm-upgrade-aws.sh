5aacc09b9fcbfe41e5ea9ffecc906d3d9a36f519
stendler <m.stendler@fu-berlin.de>
2020-07-23 00:41:05 +0200

    update helm-upgrade-aws script with new chart name
    Signed-off-by: stendler <m.stendler@fu-berlin.de>

--- helm-upgrade-aws.sh
+++ helm-upgrade-aws.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-helm upgrade federation chart/of-federation/ --values chart/of-federation/values.yaml -n openfaas --set serviceType=LoadBalancer
+helm upgrade openfaas-policy chart/of-federation/ --values chart/of-federation/values.yaml -n openfaas --set serviceType=LoadBalancer
